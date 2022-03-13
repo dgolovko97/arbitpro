e53a0717c086f810be6149ea6f994a48506c3097
d.golovko <dmitrigolovko13@gmail.com>
2022-03-14 01:11:08 +0300

    pages

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,41 @@
-const element = document.querySelector('#kt_ecommerce_sales_flatpickr');
-flatpickr = $(element).flatpickr({
-    altInput: true,
-    altFormat: "d.m.Y",
-    dateFormat: "Y.m.d",
-    mode: "range",
-    defaultDate: ["2022-02-05", "2022-03-05"]
-});
-const clearButton = document.querySelector('#kt_ecommerce_sales_flatpickr_clear');
-clearButton.addEventListener('click', e => {
-    flatpickr.clear();
-});
+var start = moment().subtract(29, "days");
+var end = moment();
+
+function cb(start, end) {
+    $("#kt_ecommerce_sales_flatpickr").html(start.format("MMMM D, YYYY") + " - " + end.format("MMMM D, YYYY"));
+    $("#kt_ecommerce_sales_flatpickr_2").html(start.format("MMMM D, YYYY") + " - " + end.format("MMMM D, YYYY"));
+}
+
+$("#kt_ecommerce_sales_flatpickr").daterangepicker({
+    startDate: start,
+    endDate: end,
+    ranges: {
+        "Сегодня": [moment(), moment()],
+        "Вчера": [moment().subtract(1, "days"), moment().subtract(1, "days")],
+        "Прошлые 7 дней ": [moment().subtract(6, "days"), moment()],
+        "Прошлые 30 дней": [moment().subtract(29, "days"), moment()],
+        "Этот месяц": [moment().startOf("month"), moment().endOf("month")],
+        "Прошлый месяц": [moment().subtract(1, "month").startOf("month"), moment().subtract(1, "month").endOf("month")]
+    }
+}, cb);
+$("#kt_ecommerce_sales_flatpickr_2").daterangepicker({
+    startDate: start,
+    endDate: end,
+    ranges: {
+        "Сегодня": [moment(), moment()],
+        "Вчера": [moment().subtract(1, "days"), moment().subtract(1, "days")],
+        "Прошлые 7 дней ": [moment().subtract(6, "days"), moment()],
+        "Прошлые 30 дней": [moment().subtract(29, "days"), moment()],
+        "Этот месяц": [moment().startOf("month"), moment().endOf("month")],
+        "Прошлый месяц": [moment().subtract(1, "month").startOf("month"), moment().subtract(1, "month").endOf("month")]
+    }
+}, cb);
+
+cb(start, end);
+
 $("#kt_datatable_example_6").DataTable({
+    responsive: true
+});
+$("#kt_datatable_example_6_2").DataTable({
     responsive: true
 });
